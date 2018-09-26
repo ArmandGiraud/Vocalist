c7832ae07d433b2ebbff281b2e0121e97d46b29d
Armand Giraud <armand.giraud.ag@gmail.com>
2018-09-26 23:11:15 +0200

    finish game

--- Vocalist/GameViewController.swift
+++ Vocalist/GameViewController.swift
@@ -18,6 +18,7 @@ class GameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     @IBOutlet weak var word_placeholder: UILabel!
     @IBOutlet weak var answer_text_filed: UITextField!
     
+    
     @IBOutlet weak var result_label: UILabel!
     @IBOutlet weak var submit_button: UIButton!
     
@@ -27,12 +28,16 @@ class GameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
     
     var selected_mode: String = "Russian to French"
     var selected_rounds: Int = 0
+    var n: Int = 0
+    
+    var expected_answer: String! = ""
     
     override func viewDidLoad() {
 
         super.viewDidLoad()
         
         makeRound()
+        get_word()
         
         modesPicker.delegate = self
         modesPicker.dataSource = self
@@ -87,6 +92,7 @@ class GameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
         }else {
             
         }
+        get_word()
         
         
     }
@@ -107,24 +113,47 @@ class GameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
         // add a if russian to french or french to russian
         // save loaded words
         meals = loadMeals()!
-        print(selected_mode)
         if selected_mode == "Russian to French" {
-            let n = Int(arc4random_uniform(UInt32(Int(meals.count))))
+            n = Int(arc4random_uniform(UInt32(Int(meals.count))))
             word_placeholder.text = meals[n].translation
+            expected_answer = meals[n].word
         }else if selected_mode == "French to Russian"{
-            let n = Int(arc4random_uniform(UInt32(Int(meals.count))))
+            n = Int(arc4random_uniform(UInt32(Int(meals.count))))
             word_placeholder.text = meals[n].word
+            expected_answer = meals[n].translation
         }else{
-            
         }
         
     }
     
+    func check_answer(){
+        if selected_mode == "Russian to French" {
+            expected_answer = meals[n].word
+        }else if selected_mode == "French to Russian"{
+            expected_answer = meals[n].translation
+        }else{
+            
+        }
+        if let expected_answer = expected_answer {
+        if answer_text_filed.text!.lowercased() == expected_answer.lowercased(){
+            result_label.text = "Good job!"
+            get_word()
+        }else{
+            result_label.text = "Try again!"
+        }
+        print(expected_answer.lowercased())
+        print(answer_text_filed.text!.lowercased())
+        print(answer_text_filed.text!.lowercased() == expected_answer.lowercased())
+        }
+    }
+    
     //MARK: Actions
     @IBAction func start_game(_ sender: Any) {
-        get_word()
     }
 
+    @IBAction func submit_action(_ sender: Any) {
+        check_answer()
+    }
     
     
     func loadMeals() -> [Meal]?  {
